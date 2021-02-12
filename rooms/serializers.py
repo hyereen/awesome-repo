abff930517f9c26eefa22f4eb0ff36a4042f07e2
Hyereen Kong <reen13@ajou.ac.kr>
2021-02-12 14:37:56 +0900

    #2.1

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -2,12 +2,23 @@ from rest_framework import serializers
 from users.serializers import UserSerializer
 from .models import Room
 
-class RoomSerializer(serializers.ModelSerializer):
+class ReadRoomSerializer(serializers.ModelSerializer):
 
     user = UserSerializer()
     class Meta:
         model = Room
-        # 포함된걸 쓰고싶을 떄 
-        # fields = ("pk", "name", "price", "user")
-        # 전체에서 포함하지 않는 걸 적고 싶을 떄
         exclude = ("modified",)
+
+class WriteRoomSerializer(serializers.Serializer):
+    
+    name = serializers.CharField(max_length=140)
+    address = serializers.CharField(max_length=140)
+    price = serializers.IntegerField()
+    beds = serializers.IntegerField(default=1)
+    lat = serializers.DecimalField(max_digits=10, decimal_places=6)
+    lng = serializers.DecimalField(max_digits=10, decimal_places=6)
+    bedrooms = serializers.IntegerField(default=1)
+    bathrooms = serializers.IntegerField(default=1)
+    check_in = serializers.TimeField(default="00:00:00")
+    check_out = serializers.TimeField(default="00:00:00")
+    instant_book = serializers.BooleanField(default=False)
