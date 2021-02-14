ecfbdae116a11827de35d8eef48c5669647cf83e
Hyereen Kong <reen13@ajou.ac.kr>
2021-02-14 13:43:23 +0900

    #2.16

--- rooms/views.py
+++ rooms/views.py
@@ -85,11 +85,13 @@ class RoomView(APIView):
 
 @api_view(["GET"])
 def room_search(request):
-    max_price = request.GET.get('max_price', None)
-    min_price = request.GET.get('min_price', None)
-    beds = request.GET.get('beds', None)
-    bedrooms = request.GET.get('bedrooms', None)
-    bathrooms = request.GET.get('bathrooms', None)
+    max_price = request.GET.get("max_price", None)
+    min_price = request.GET.get("min_price", None)
+    beds = request.GET.get("beds", None)
+    bedrooms = request.GET.get("bedrooms", None)
+    bathrooms = request.GET.get("bathrooms", None)
+    lat = request.GET.get("lat", None)
+    lng = request.GET.get("lng", None)
 
     filter_kwargs = {}
     if max_price is not None:
@@ -105,6 +107,13 @@ def room_search(request):
 
     paginator = PageNumberPagination()
     paginator.page_size = 10
+
+    if lat is not None and lng is not None:
+        filter_kwargs["lat__gte"] = float(lat) - 0.005
+        filter_kwargs["lat__lte"] = float(lat) + 0.005
+        filter_kwargs["lng__gte"] = float(lng) - 0.005
+        filter_kwargs["lng__lte"] = float(lng) + 0.005
+
 
     try:
         rooms = Room.objects.filter(**filter_kwargs)
